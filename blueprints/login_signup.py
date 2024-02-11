3308c93d09d2ec2f2fac67ca46b324baf446049b
Kaustubh Mayekar <67229974+Kaustubh01@users.noreply.github.com>
2024-02-11 16:48:30 +0530

    fixed links

--- blueprints/login_signup.py
+++ blueprints/login_signup.py
@@ -5,3 +5,7 @@ login_blueprint = Blueprint('login',__name__)
 @login_blueprint.route('/login')
 def login():
     return render_template('login.html')
+
+@login_blueprint.route('/signup')
+def signup():
+    return render_template('signup.html')
