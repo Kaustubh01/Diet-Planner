c4b885ac3edec56681276673dd3b0b656d177479
Kaustubh <kaustubhmayekar02@gmal.com>
2024-03-09 03:24:28 +0530

    added knn model

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import pickle
+import numpy as np
+
 from flask import Flask, render_template, session
 from blueprints.login_signup import login_blueprint
 from blueprints.result import result_blueprint
@@ -12,14 +15,22 @@ app.register_blueprint(login_blueprint)
 app.register_blueprint(result_blueprint)
 app.register_blueprint(dashboard_blueprint)
 
+
 @app.route('/')
 def hello():
     images = ['image1.jpg', 'image2.jpg', 'image3.jpg']
-    return render_template('index.html', images = images)
+    return render_template('index.html', images=images)
+
 
 @app.route('/bmi')
 def bmi():
-    return render_template('bmi.html')
+    with open('knn_classifier', 'rb') as file:
+        loaded_model = pickle.load(file)
+        predictions = loaded_model.predict(np.array([144]).reshape(1,-1))
+    return render_template('bmi.html',predictions = predictions)
+
+
+# Load the model from the saved fil
 
 if __name__ == '__main__':
     app.run(debug=True)
